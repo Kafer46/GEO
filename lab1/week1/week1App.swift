073925e689b7fa17c649353fb3720d38568b6d44
Kafer46 <95630830+Kafer46@users.noreply.github.com>
2026-09-21 14:18:56 +0700

    Add files via upload

---
+++ lab1/week1/week1App.swift
@@ -0,0 +1,17 @@
+//
+//  week1App.swift
+//  week1
+//
+//  Created by MAY 04 on 21/9/26.
+//
+
+import SwiftUI
+
+@main
+struct week1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
