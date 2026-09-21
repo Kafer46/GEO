ed1200da97f383bb81dffaed6ea412269d480e23
Kafer46 <95630830+Kafer46@users.noreply.github.com>
2026-09-21 14:05:59 +0700

    Add files via upload

---
+++ week1/week1UITests/week1UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  week1UITestsLaunchTests.swift
+//  week1UITests
+//
+//  Created by MAY 04 on 21/9/26.
+//
+
+import XCTest
+
+final class week1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
