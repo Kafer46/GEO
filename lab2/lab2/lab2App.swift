0b5cf142cd69f6a6f80a2e48df94c04cde006fd0
Kafer46 <95630830+Kafer46@users.noreply.github.com>
2026-09-21 15:50:22 +0700

    21/9/2026

---
+++ lab2/lab2/lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  lab2App.swift
+//  lab2
+//
+//  Created by MAY 04 on 21/9/26.
+//
+
+import SwiftUI
+
+@main
+struct lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
